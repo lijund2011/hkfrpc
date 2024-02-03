ac21b91427ce6f540b61c206d724a474477fc1b9
lijund2011 <77002051+lijund2011@users.noreply.github.com>
2024-02-04 01:14:39 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,155 @@
 
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+# fonts color
+Green="\033[32m"
+Red="\033[31m"
+Yellow="\033[33m"
+GreenBG="\033[42;37m"
+RedBG="\033[41;37m"
+Font="\033[0m"
+# fonts color
+
+# variable
+WORK_PATH=$(dirname $(readlink -f $0))
+FRP_NAME=frpc
+FRP_VERSION=0.51.3
+FRP_PATH=/usr/local/frp
+PROXY_URL="https://ghproxy.com/"
+author=lijund2011
+FRP_core=hkfrp
+FRP_core_name=HKFRP
+FRP_core_dir=/etc/$FRP_core
+FRP_sh_dir=$FRP_core_dir
+
+
+# print a mesage
+msg() {
+    case $1 in
+    warn)
+        local color=$yellow
+        ;;
+    err)
+        local color=$red
+        ;;
+    ok)
+        local color=$green
+        ;;
+    esac
+
+    echo -e "${color}$(date +'%T')${Font}) ${2}"
+}
+
+# download file
+download() {
+# check network
+GOOGLE_HTTP_CODE=$(curl -o /dev/null --connect-timeout 5 --max-time 8 -s --head -w "%{http_code}" "https://www.google.com")
+PROXY_HTTP_CODE=$(curl -o /dev/null --connect-timeout 5 --max-time 8 -s --head -w "%{http_code}" "${PROXY_URL}")
+    case $1 in
+    sh)
+        if [ $GOOGLE_HTTP_CODE == "200" ]; then
+    wget -P ${WORK_PATH} https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/${FILE_NAME}.tar.gz -O ${FILE_NAME}.tar.gz
+	name="${FILE_NAME}.tar.gz"
+	link=${WORK_PATH} https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/${FILE_NAME}.tar.gz -O ${FILE_NAME}.tar.gz
+else
+    if [ $PROXY_HTTP_CODE == "200" ]; then
+        wget -P ${WORK_PATH} ${PROXY_URL}https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/${FILE_NAME}.tar.gz -O ${FILE_NAME}.tar.gz
+		name="代理下载${FILE_NAME}.tar.gz"
+	    link=${WORK_PATH} https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/${FILE_NAME}.tar.gz -O ${FILE_NAME}.tar.gz
+    else
+        echo -e "${Red}检测 GitHub Proxy 代理失效 开始使用官方地址下载${Font}"
+        wget -P ${WORK_PATH} https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/${FILE_NAME}.tar.gz -O ${FILE_NAME}.tar.gz
+    fi
+fi
+tar -zxvf ${FILE_NAME}.tar.gz
+
+mkdir -p ${FRP_PATH}
+chmod -R 755 ${FRP_PATH}
+mv ${FILE_NAME}/${FRP_NAME} ${FRP_PATH}
+        ;;
+	jq)
+        link=https://github.com/jqlang/jq/releases/download/jq-1.7rc1/jq-linux-$is_jq_arch
+        name="jq"
+        tmpfile=$tmpjq
+        is_ok=$is_jq_ok
+        ;;
+    esac
+
+    msg warn "下载 ${name} > ${link}"
+}
+
+# main
+main() {
+# check frpc
+if [ -f "/usr/local/frp/${FRP_NAME}" ] || [ -f "/usr/local/frp/${FRP_NAME}.ini" ] || [ -f "/lib/systemd/system/${FRP_NAME}.service" ];then
+    echo -e "${Green}=========================================================================${Font}"
+    echo -e "${RedBG}当前已退出脚本.${Font}"
+    echo -e "${Green}检查到服务器已安装${Font} ${Red}${FRP_NAME}${Font}"
+    echo -e "${Green}请手动确认和删除${Font} ${Red}/usr/local/frp/${Font} ${Green}目录下的${Font} ${Red}${FRP_NAME}${Font} ${Green}和${Font} ${Red}/${FRP_NAME}.ini${Font} ${Green}文件以及${Font} ${Red}/lib/systemd/system/${FRP_NAME}.service${Font} ${Green}文件,再次执行本脚本.${Font}"
+    echo -e "${Green}参考命令如下:${Font}"
+    echo -e "${Red}rm -rf /usr/local/frp/${FRP_NAME}${Font}"
+    echo -e "${Red}rm -rf /usr/local/frp/${FRP_NAME}.ini${Font}"
+    echo -e "${Red}rm -rf /lib/systemd/system/${FRP_NAME}.service${Font}"
+    echo -e "${Green}=========================================================================${Font}"
+    exit 0
+fi
+
+while ! test -z "$(ps -A | grep -w ${FRP_NAME})"; do
+    FRPCPID=$(ps -A | grep -w ${FRP_NAME} | awk 'NR==1 {print $1}')
+    kill -9 $FRPCPID
+done
+
+# check pkg
+if type apt-get >/dev/null 2>&1 ; then
+    if ! type wget >/dev/null 2>&1 ; then
+        apt-get install wget -y
+    fi
+    if ! type curl >/dev/null 2>&1 ; then
+        apt-get install curl -y
+    fi
+fi
+
+if type yum >/dev/null 2>&1 ; then
+    if ! type wget >/dev/null 2>&1 ; then
+        yum install wget -y
+    fi
+    if ! type curl >/dev/null 2>&1 ; then
+        yum install curl -y
+    fi
+fi
+
+# check network
+GOOGLE_HTTP_CODE=$(curl -o /dev/null --connect-timeout 5 --max-time 8 -s --head -w "%{http_code}" "https://www.google.com")
+PROXY_HTTP_CODE=$(curl -o /dev/null --connect-timeout 5 --max-time 8 -s --head -w "%{http_code}" "${PROXY_URL}")
+
+# check arch
+if [ $(uname -m) = "x86_64" ]; then
+    PLATFORM=amd64
+elif [ $(uname -m) = "aarch64" ]; then
+    PLATFORM=arm64
+elif [ $(uname -m) = "armv7" ]; then
+    PLATFORM=arm
+elif [ $(uname -m) = "armv7l" ]; then
+    PLATFORM=arm
+elif [ $(uname -m) = "armhf" ]; then
+    PLATFORM=arm
+fi
+
+FILE_NAME=frp_${FRP_VERSION}_linux_${PLATFORM}
+
+# check old version
+    [[ -f $FRP_core_dir && -d $FRP_PATH ]] && {
+        err "检测到脚本已安装, 如需重装请使用${green} ${is_core} reinstall ${none}命令."
+    }
+
+# show welcome msg
+    clear
+    echo
+    echo "........... $FRP_core_name script by $author .........."
+    echo
+	download sh
+}
+# start.
+main $@
