e6941f69c65466857733c04896ffd52f893a401a
lijund2011 <77002051+lijund2011@users.noreply.github.com>
2024-02-04 01:44:48 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -79,12 +79,9 @@ else
 	    link=https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/${FILE_NAME}.tar.gz -O ${FILE_NAME}.tar.gz
 	    #msg warn "下载 ${name} > ${link}"
         wget -P ${WORK_PATH} ${PROXY_URL}https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/${FILE_NAME}.tar.gz -O ${FILE_NAME}.tar.gz
-		
     else
         echo -e "${Red}检测 GitHub Proxy 代理失效 开始使用官方地址下载${Font}"
         wget -P ${WORK_PATH} https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/${FILE_NAME}.tar.gz -O ${FILE_NAME}.tar.gz
-    fi
-
         ;;
 	jq)
         link=https://github.com/jqlang/jq/releases/download/jq-1.7rc1/jq-linux-$is_jq_arch
